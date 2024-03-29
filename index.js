8806d3a4fc263c2dee67f11b1de8c50e35572011
Dan Jacoby <jacobydan01@gmail.com>
2024-03-29 12:01:17 -0400

    event

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-function addingEventListener() {
-    const input = document.getElementById('button');
 
-function clickAlert() {
-  alert('I was clicked!');
-}
+//not sure why this code format doesn't work
+// function addingEventListener() {
+//     const input = document.getElementById('button');
 
-input.addEventListener('click', clickAlert);  
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);  
+// }
+
+
+//selects element with id #button, ads a click event, triggers function
+document.querySelector('#button').addEventListener('click', clickAlert)
+
+//function sends alert that says I was clicked
+function clickAlert(){
+  alert("I was clicked")
 }
