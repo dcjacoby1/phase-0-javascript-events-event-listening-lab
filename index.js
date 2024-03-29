abe927aae6084f288d28148c6e3f2e3bcbbd753e
Dan Jacoby <jacobydan01@gmail.com>
2024-03-29 12:08:22 -0400

    events

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 
-//not sure why this code format doesn't work
-// function addingEventListener() {
-//     const input = document.getElementById('button');
+//function selects id 'button'
+function addingEventListener() {
+    const input = document.getElementById('button');
 
-// function clickAlert() {
-//   alert('I was clicked!');
-// }
-
-// input.addEventListener('click', clickAlert);  
-// }
-
-
-//selects element with id #button, ads a click event, triggers function
-document.querySelector('#button').addEventListener('click', clickAlert)
-
-//function sends alert that says I was clicked
-function clickAlert(){
-  alert("I was clicked")
+//function creates alert
+function clickAlert() {
+  alert('I was clicked!');
 }
+//takes the input from add event listener, initializes click event, that trickers clickAlert message
+//only works bc it is in scope of adding event listener
+input.addEventListener('click', clickAlert);  
+}
+//triggers function that allows event to occur
+addingEventListener()
+
+
+// //this code works
+// //selects element with id #button, ads a click event, triggers function
+// document.querySelector('#button').addEventListener('click', clickAlert)
+
+// //function sends alert that says I was clicked
+// function clickAlert(){
+//   alert("I was clicked")
+// }
